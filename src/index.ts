cd90cbc96a2f7c72f7135d9d7a3563ffd2d83f2c
Brandon Do <do.brandon97@gmail.com>
2024-12-26 00:24:26 -0500

    render basic images

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import { Notice, Plugin } from 'obsidian';
+import { Plugin } from 'obsidian';
 
 export default class Gallery extends Plugin {
   onload(): Promise<void> | void {
-    this.addRibbonIcon('dice', 'Greet', () => new Notice('Hello World!'));
+    this.registerMarkdownCodeBlockProcessor('gallery', (source, el, ctx) => {
+      const container = el.createDiv({
+        attr: { display: 'block' },
+      });
+      const rows = source.split('\n').filter((row) => row.length > 0);
+
+      for (let i = 0; i < rows.length; i++) {
+        const imgPath = this.app.vault.adapter.getResourcePath(rows[i]);
+        container.append(
+          el.createEl('img', {
+            attr: { src: imgPath, alt: rows[i] },
+          })
+        );
+      }
+    });
   }
 
   onunload(): void {}
